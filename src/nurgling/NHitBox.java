62d07d780d3e6de7a6cfe57383d335879b9a206a
Alex Svoboda <40924818+aleksandrsvoboda@users.noreply.github.com>
2026-02-23 23:16:31 -0700

    fix/gardenpot placement hitbox (#27)
    Co-authored-by: Developer <dev@nurgling.local>

--- src/nurgling/NHitBox.java
+++ src/nurgling/NHitBox.java
@@ -57,7 +57,7 @@ public class NHitBox
             put("gfx/terobjs/pclaim", new NHitBox(new Coord(-3,-3),new Coord(3,3)));
             put("gfx/terobjs/iconsign", new NHitBox(new Coord(-2,-2),new Coord(2,2)));
             put("gfx/terobjs/candelabrum", new NHitBox(new Coord(-2,-2),new Coord(2,2)));
-            put("gfx/terobjs/gardenpot", new NHitBox(new Coord(-2,-2),new Coord(2,2)));
+            put("gfx/terobjs/gardenpot", new NHitBox(new Coord2d(-2.5,-2.5), new Coord2d(2.5,2.5), true));
             put("gfx/terobjs/cupboard", new NHitBox(new Coord2d(-5.5,-5.5),new Coord2d(5.5,5.5)));
             put("gfx/terobjs/htable", new NHitBox(new Coord2d(-3.5,-7.0),new Coord2d(3.5,7.0), true));
             put("gfx/terobjs/lanternpost", new NHitBox(new Coord(-2,-2),new Coord(2,2)));
