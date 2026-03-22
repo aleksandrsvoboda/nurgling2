e26e00039051008ed467d2b2cd8c86823f00128c
Developer <dev@nurgling.local>
2026-03-21 19:53:30 -0700

    hitbox

--- src/nurgling/NHitBox.java
+++ src/nurgling/NHitBox.java
@@ -139,7 +139,7 @@ public class NHitBox
             put("gfx/terobjs/producesack-closed3", new NHitBox(new Coord2d(-4,-4),new Coord2d(4,4)));
             put("gfx/terobjs/arch/belltower", new NHitBox(new Coord2d(-15,-19),new Coord2d(15,19)));
             put("gfx/terobjs/still", new NHitBox(new Coord2d(-12,-6),new Coord2d(5,6)));
-            put("gfx/terobjs/grandstudydesk", new NHitBox(new Coord2d(-6.12841796875,-11.7412109375),new Coord2d(7.35302734375,11.7412109375)));
+            put("gfx/terobjs/grandstudydesk", new NHitBox(new Coord2d(-6.2,-11.75),new Coord2d(7.45,11.75)));
         }
     };
     static NHitBox fromObstacle(Coord2d[][] p)
